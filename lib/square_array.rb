f322fcf645b163c374bf827b333d3211989b53c4
James Landry <james.j.landry@outlook.com>
2020-05-19 17:05:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
     count = 0
     new_array = []
     while count < array.length do
-      new_array.index(array ** 2)
+      new_array.index(array[index] ** 2)
      count += 1
     end
   
